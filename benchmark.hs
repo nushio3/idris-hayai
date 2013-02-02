d546f0001d602045f6ed3736f7054885798218f3
Takayuki Muranushi <muranushi@gmail.com>
2013-02-02 11:33:16 +0900

    benchmark run

--- benchmark.hs
+++ benchmark.hs
@@ -1,9 +1,11 @@
 #!/usr/bin/env runhaskell
 import Control.Monad
-import Data.Time
+import Data.Time (getCurrentTime, diffUTCTime)
 import Data.String.Utils (replace)
+import System.IO
 import System.IO.Unsafe (unsafePerformIO)
-import System.Process (system)
+import System.Process (system, runInteractiveCommand)
+import Text.Printf(printf)
 
 
 {-# NOINLINE idrisTemplate #-}
@@ -14,7 +16,33 @@ idrisTemplate =
 
 
 main = do
+  fp  <- openFile "bench-idr.txt" WriteMode
+  fp2 <- openFile "bench-hs.txt" WriteMode
   forM (map (2^) [0..20]) $ \n ->do
     writeFile "sum-gen.idr" $
       replace "<<<n>>>" (show n) idrisTemplate
     system "make sum-gen-idr"
+    beginTime <- getCurrentTime
+    (_, hOut,_,_) <- runInteractiveCommand "./sum-gen-idr"
+    outStr <- hGetContents hOut
+    putStr outStr
+    endTime <- getCurrentTime
+
+    beginTime2 <- getCurrentTime
+    (_, hOut2,_,_) <- runInteractiveCommand $
+                     printf "./sum-hs %d" n
+    outStr2 <- hGetContents hOut2
+    putStr outStr2
+    endTime2 <- getCurrentTime
+
+    let diffUTC  =diffUTCTime endTime beginTime
+        diff, diff2 :: Double
+        diff  = fromRational $ toRational diffUTC
+        diff2 = fromRational $ toRational $
+          diffUTCTime endTime2 beginTime2
+    hPutStr fp $ printf "%d\t%f\n" (n::Integer) diff
+    hFlush fp
+    hPutStr fp2 $ printf "%d\t%f\n" (n::Integer) diff2
+    hFlush fp2
+  hClose fp
+  hClose fp2
--- sum.hs
+++ sum.hs
@@ -1,3 +1,4 @@
+import System.Environment
 
 sumAcc :: Int -> Int -> Int
 sumAcc n accum = if n <=0 then accum else sumAcc (n-1) $ n+accum
@@ -5,4 +6,6 @@ sumAcc n accum = if n <=0 then accum else sumAcc (n-1) $ n+accum
 sumUpto :: Int -> Int
 sumUpto n = sumAcc n 0
 
-main = print $ sumUpto 1234567
+main = do
+  n <- fmap (read . head) $ getArgs
+  print $ sumUpto n
